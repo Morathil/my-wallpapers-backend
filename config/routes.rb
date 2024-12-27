1627e9a643c6a16e58e08689a7c9b9e8252da7b9
Christoph Altmann <christoph.altmann@senseven.ai>
2024-12-27 13:10:25 +0100

    Set sidekiq as queue adapter

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
@@ -15,6 +17,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  mount Sidekiq::Web => '/sidekiq' # Sidekiq web interface at /sidekiq
 
   # Defines the root path route ("/")
   # root "posts#index"
