79b4ef4a068900e66287a1e00832c31dfb399108
Christoph Altmann <christoph.altmann@senseven.ai>
2024-12-27 16:20:03 +0100

    Disable session_store

--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,7 @@ module MyWallpapersBackend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.session_store :cookie_store, key: '_interslice_session'
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use config.session_store, config.session_options    
+    config.session_store :disabled 
 
     config.active_job.queue_adapter = :sidekiq
   end
