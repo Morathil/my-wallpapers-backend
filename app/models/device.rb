dc9e195ac550c8c9273020e6625a35a37d528593
Christoph Altmann <christoph.altmann@senseven.ai>
2024-12-28 12:24:58 +0100

    Add is_portrait helper to device model

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,4 +1,8 @@
 class Device < ApplicationRecord
-    belongs_to :user
-    has_many :image_groups
+  belongs_to :user
+  has_many :image_groups
+
+  def is_portrait
+    self.height > self.width
+  end
 end
