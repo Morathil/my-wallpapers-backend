c6da29b019437d333c04937872610b17577d475c
Christoph Altmann <christoph.altmann@senseven.ai>
2025-01-02 17:13:42 +0100

    Add attr_reader to image_group_service

--- app/services/image_group_service.rb
+++ app/services/image_group_service.rb
@@ -1,23 +1,25 @@
 class ImageGroupService
+  attr_reader :image_group, :device, :original_image
+
   def initialize(image_group_id)
     @image_group = ImageGroup.find(image_group_id)
-    @original_image = @image_group.original_image
-    @device = @image_group.device
+    @original_image = image_group.original_image
+    @device = image_group.device
   end
 
   def create_cropped_and_thumbnail_images_by_crop_hints
-    target_wallpaper_orientation = @image_group.device.portrait? ? :portrait : :landscape
+    target_wallpaper_orientation = image_group.device.portrait? ? :portrait : :landscape
 
-    @original_image.file.open do |file|
+    original_image.file.open do |file|
       imageProcessor = ImageProcessing::ImageProcessor.new(file.path)
-      generated_cropped = imageProcessor.generate_cropped(device_width: @device.width, device_height: @device.height, target_wallpaper_orientation:)
+      generated_cropped = imageProcessor.generate_cropped(device_width: device.width, device_height: device.height, target_wallpaper_orientation:)
       generated_thumbnail = imageProcessor.generate_thumbnail
 
       ActiveRecord::Base.transaction do
         cropped_image = create_image_by_type(image_data: generated_cropped, image_type: Image.image_types[:cropped])
         thumbnail_image = create_image_by_type(image_data: generated_thumbnail, image_type: Image.image_types[:thumbnail])
 
-        @image_group.update(cropped_id: cropped_image.id, thumbnail_id: thumbnail_image.id)
+        image_group.update(cropped_id: cropped_image.id, thumbnail_id: thumbnail_image.id)
       end
     end
   end
