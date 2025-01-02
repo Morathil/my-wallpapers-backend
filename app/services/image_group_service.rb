93c6529695c2516601e90af1360ab912316220e4
Christoph Altmann <christoph.altmann@senseven.ai>
2025-01-02 16:00:47 +0100

    Refactor by splitting generate_cropped_and_thumbnail into generate_cropped and generate_thumbnail

--- app/services/image_group_service.rb
+++ app/services/image_group_service.rb
@@ -10,19 +10,12 @@ class ImageGroupService
 
     @original_image.file.open do |file|
       imageProcessor = ImageProcessing::ImageProcessor.new(file.path)
-      generated_cropped_and_thumbnail = imageProcessor.generate_cropped_and_thumnail(device_width: @device.width, device_height: @device.height, target_wallpaper_orientation:)
-
-      cropped_blob = generated_cropped_and_thumbnail[:cropped_blob]
-      cropped_width = generated_cropped_and_thumbnail[:cropped_width]
-      cropped_height = generated_cropped_and_thumbnail[:cropped_height]
-
-      thumbnail_blob = generated_cropped_and_thumbnail[:thumbnail_blob]
-      thumbnail_width = generated_cropped_and_thumbnail[:thumbnail_width]
-      thumbnail_height = generated_cropped_and_thumbnail[:thumbnail_height]
+      generated_cropped = imageProcessor.generate_cropped(device_width: @device.width, device_height: @device.height, target_wallpaper_orientation:)
+      generated_thumbnail = imageProcessor.generate_thumnail
 
       ActiveRecord::Base.transaction do
-        cropped_image = create_image_by_type(image_blob: cropped_blob, width: cropped_width, height: cropped_height, image_type: Image.image_types[:cropped])
-        thumbnail_image = create_image_by_type(image_blob: thumbnail_blob, width: thumbnail_width, height: thumbnail_height, image_type: Image.image_types[:thumbnail])
+        cropped_image = create_image_by_type(image_data: generated_cropped, image_type: Image.image_types[:cropped])
+        thumbnail_image = create_image_by_type(image_data: generated_thumbnail, image_type: Image.image_types[:thumbnail])
 
         @image_group.update(cropped_id: cropped_image.id, thumbnail_id: thumbnail_image.id)
       end
@@ -31,8 +24,8 @@ class ImageGroupService
 
   private
 
-  def create_image_by_type(image_blob:, width:, height:, image_type:)
-    image = Image.new(width: width, height: height, image_type: image_type)
+  def create_image_by_type(image_data:, image_type:)
+    image = Image.new(width: image_data[:width], height: image_data[:height], image_type: image_type)
     image.file.attach(image_blob)
     image.save!
     image
--- app/services/image_processing/image_processor.rb
+++ app/services/image_processing/image_processor.rb
@@ -2,18 +2,20 @@ require "vips"
 
 module ImageProcessing
   class ImageProcessor
+    attr_reader :vip_image, :cropped_vip_image, :image_width, :image_height
+
     def initialize(filePath)
-      @image = ::Vips::Image.new_from_file(filePath)
-      @image_width = @image.width.to_f # convert to float for division
-      @image_height = @image.height.to_f # convert to float for division
+      @vip_image = ::Vips::Image.new_from_file(filePath)
+      @image_width = @vip_image.width.to_f # convert to float for division
+      @image_height = @vip_image.height.to_f # convert to float for division
     end
 
     def get_size
-      { width: @image_width, height: @image_height }
+      { width: image_width, height: image_height }
     end
 
-    def generate_cropped_and_thumnail(device_width:, device_height:, target_wallpaper_orientation:)
-      new_dimensions = ImageDimensions.new(vip_image: @image, device_width:, device_height:, new_wallpaper_orientation:)
+    def generate_cropped(device_width:, device_height:, target_wallpaper_orientation:)
+      new_dimensions = ImageDimensions.new(vip_image:, device_width:, device_height:, new_wallpaper_orientation:)
 
       cropped_width = new_dimensions.target_width
       cropped_height = new_dimensions.target_height
@@ -21,13 +23,13 @@ module ImageProcessing
       # Scale Down To Cropped Size (not cropped yet)
       device_size_scaling_factor = 1
 
-      if @image_height > cropped_height && target_wallpaper_orientation == :portrait
-        device_size_scaling_factor = cropped_height / @image_height
-      elsif @image_width > cropped_width
-        device_size_scaling_factor = cropped_width / @image_width
+      if image_height > cropped_height && target_wallpaper_orientation == :portrait
+        device_size_scaling_factor = cropped_height / image_height
+      elsif image_width > cropped_width
+        device_size_scaling_factor = cropped_width / image_width
       end
 
-      device_size_image_resizer = ImageProcessing::ImageResizer.new(@image)
+      device_size_image_resizer = ImageProcessing::ImageResizer.new(vip_image)
       device_size_image = device_size_image_resizer.resize_by(factor: device_size_scaling_factor)
 
       device_size_image_buffer = device_size_image.write_to_buffer(".jpg")
@@ -42,28 +44,35 @@ module ImageProcessing
       image_cropper = ImageProcessing::ImageCropper.new(image: device_size_image, cropped_height:, cropped_width:, crop_center_y:, crop_center_x:, target_wallpaper_orientation:)
       cropped_image = image_cropper.crop
 
-      # Thumnail
-      thumbnail_image_resizer = ImageProcessing::ImageResizer.new(cropped_image)
-      thumbnail_image = thumbnail_image_resizer.resize_to(pixel: 320) # 0.3 scaling factor
+      cropped_vip_image = cropped_image
 
       cropped_blob_data = cropped_image.write_to_buffer(".jpg")
-      thumbnail_blob_data = thumbnail_image.write_to_buffer(".jpg")
 
       cropped_blob = ActiveStorage::Blob.create_and_upload!(
         io: StringIO.new(cropped_blob_data),  # Use StringIO for in-memory processing
         filename: "cropped_#{Time.now.to_i}.jpg",
         content_type: "image/jpeg"
       )
+
+      {
+        blob: cropped_blob, width: cropped_image.width, height: cropped_image.height
+      }
+    end
+
+    def generate_thumbnail()
+      thumbnail_image_resizer = ImageProcessing::ImageResizer.new(cropped_vip_image)
+      thumbnail_image = thumbnail_image_resizer.resize_to(pixel: 320) # 0.3 scaling factor
+
+      thumbnail_blob_data = thumbnail_image.write_to_buffer(".jpg")
       thumbnail_blob = ActiveStorage::Blob.create_and_upload!(
         io: StringIO.new(thumbnail_blob_data),  # Use StringIO for in-memory processing
         filename: "thumbnail_#{Time.now.to_i}.jpg",
         content_type: "image/jpeg"
       )
-
+      
       {
-        cropped_blob: cropped_blob, cropped_width: cropped_image.width, cropped_height: cropped_image.height,
-        thumbnail_blob: thumbnail_blob, thumbnail_width: thumbnail_image.width, thumbnail_height: thumbnail_image.height
-      }
+        blob: thumbnail_blob, width: thumbnail_image.width, height: thumbnail_image.height
+      }      
     end
   end
 end
